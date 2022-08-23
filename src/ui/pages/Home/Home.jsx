231556fd85f2423df69e966a8b9953871a166a09
0785675371 <107850307+0785675371@users.noreply.github.com>
2022-08-23 09:18:46 +0200

    sidsdfjsf

--- src/ui/pages/Home/Home.jsx
+++ src/ui/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import './Home.css'
+import Contactus from '../../components/Contactus/Contactus'
 
 
 export default function Home() {
   return (
     <div>
+      <Contactus />
       Home
     </div>
   )
