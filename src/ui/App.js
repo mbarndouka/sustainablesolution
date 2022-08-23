e9c7543d18a4e38fa1f5d3279fb6320db07839fe
mbarndouka <mmbarndouka@gmail.com>
2022-08-23 08:57:07 +0200

    Setup signin and signup

--- src/ui/App.js
+++ src/ui/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Notfound from './pages/Notfound/Notfound';
 import Loading from './pages/Loading/Loading';
+import Signin from './pages/signin/Signin';
+import Signup from './pages/Signup/Signup';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/Loading' element={<Loading />} />
         <Route path='/' element ={< Home />}/>
+        <Route path='/Signup' element = {<Signup/>}/>
+        <Route path='/Signin' element = {<Signin/>}/>
         <Route path='*' element = {<Notfound/>}/>
       </Routes>
     </BrowserRouter>
