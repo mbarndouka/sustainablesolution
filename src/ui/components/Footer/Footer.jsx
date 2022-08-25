fd67e6e188b54e1d4565b13075a20521057a3742
mbarndouka <mmbarndouka@gmail.com>
2022-08-26 00:45:25 +0200

    Footer footer done

--- src/ui/components/Footer/Footer.jsx
+++ src/ui/components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 import './Footer.css'
+import Logo from '../../../assets/Vector1.svg'
+import Button from '../button/index'
 
 export class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+      <div className='Footer'>
+        <div className="footer-first">
+          <img src={Logo} alt="" />
+          <form className='send-info'>
+            <input placeholder='Email address'/>
+            <Button>Submit</Button>
+          </form>
+        </div>
+        <div className="footer-second">
+          <h1>About</h1>
+          <p>Team</p>
+          <p>How Educat works</p>
+        </div>
+      </div>
     )
   }
 }
