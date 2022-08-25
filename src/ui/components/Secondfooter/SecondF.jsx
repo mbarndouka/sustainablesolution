23df1a73688372629d33bd58c1aa8992492053c5
mbarndouka <mmbarndouka@gmail.com>
2022-08-26 01:32:13 +0200

    The second footer

---
+++ src/ui/components/Secondfooter/SecondF.jsx
@@ -0,0 +1,11 @@
+import './SecondF.css'
+import Copy from '../../../assets/copy.svg'
+
+export default function Home() {
+    return(
+        <div className="footer2">
+            <img src={Copy} alt="a" />
+            <span>Copyright</span>
+        </div>
+    )
+}
--- src/ui/pages/signin/Signin.jsx
+++ src/ui/pages/signin/Signin.jsx
@@ -1,9 +1,11 @@
 import './Signin.css';
+import SecondF from '../../components/Secondfooter/SecondF'
 
 export default function Signin() {
   return (
     <div>
-        Signin 
+      Signin 
+      <SecondF />
     </div>
   )
 }
