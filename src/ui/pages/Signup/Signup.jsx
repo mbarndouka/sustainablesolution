3cbb2fe1225969c398f397f9243d56b745938a19
mbarndouka <mmbarndouka@gmail.com>
2022-08-26 08:09:01 +0200

    Signup padding

--- src/ui/pages/Signup/Signup.jsx
+++ src/ui/pages/Signup/Signup.jsx
@@ -1,13 +1,14 @@
 import './Signup.css';
-import SecondF from '../../components/Secondfooter/SecondF'
+// import SecondF from '../../components/Secondfooter/SecondF'
 import Logo from '../../../assets/Vector2.svg'
 
 export default function Signup() {
   return (
     <div className="Signup">
-        <img src={Logo} alt="" />
-        <div className=" ">
-
+        {/* <img src={Logo} alt="" /> */}
+        <div className="signup-body">
+          <h1>Join as a sponsor or a school</h1>
+          <form></form>
         </div>
         <SecondF />
     </div>
