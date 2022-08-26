dabce7883f6ae41e6016f58f0f739825f5f7eb15
mbarndouka <mmbarndouka@gmail.com>
2022-08-26 10:00:10 +0200

    Square done for signup page

--- src/ui/pages/Signup/Signup.jsx
+++ src/ui/pages/Signup/Signup.jsx
@@ -1,6 +1,7 @@
 import './Signup.css';
-import SecondF from '../../components/Secondfooter/SecondF'
-import Logo from '../../../assets/Vector2.svg'
+import SecondF from '../../components/Secondfooter/SecondF';
+import Logo from '../../../assets/Vector2.svg';
+import Square from '../../components/Square/index';
 import { Link } from 'react-router-dom';
 
 export default function Signup() {
@@ -10,7 +11,8 @@ export default function Signup() {
         <div className="signup-body">
           <h1>Join as a sponsor or a school</h1>
           <form className="body-form">
-            hello
+            <Square></Square>
+            <Square> </Square>
           </form>
           <p>Already have an account? <Link to="/Signin" className='connect'>Log in</Link> </p>
         </div>
