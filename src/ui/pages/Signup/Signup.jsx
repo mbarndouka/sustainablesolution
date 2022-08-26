f17e1b73a52750edab801d06e6cfa55295e10242
mbarndouka <mmbarndouka@gmail.com>
2022-08-26 08:29:28 +0200

    uncomment

--- src/ui/pages/Signup/Signup.jsx
+++ src/ui/pages/Signup/Signup.jsx
@@ -1,11 +1,11 @@
 import './Signup.css';
-// import SecondF from '../../components/Secondfooter/SecondF'
+import SecondF from '../../components/Secondfooter/SecondF'
 import Logo from '../../../assets/Vector2.svg'
 
 export default function Signup() {
   return (
     <div className="Signup">
-        {/* <img src={Logo} alt="" /> */}
+        <img src={Logo} alt="" />
         <div className="signup-body">
           <h1>Join as a sponsor or a school</h1>
           <form></form>
