a4d8abdffd8b60488a89a485a188c2e6073cfd71
mbarndouka <mmbarndouka@gmail.com>
2022-08-26 09:23:41 +0200

    Design signup but not all

--- src/ui/pages/Signup/Signup.jsx
+++ src/ui/pages/Signup/Signup.jsx
@@ -1,6 +1,7 @@
 import './Signup.css';
 import SecondF from '../../components/Secondfooter/SecondF'
 import Logo from '../../../assets/Vector2.svg'
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
   return (
@@ -8,7 +9,10 @@ export default function Signup() {
         <img src={Logo} alt="" />
         <div className="signup-body">
           <h1>Join as a sponsor or a school</h1>
-          <form></form>
+          <form className="body-form">
+            hello
+          </form>
+          <p>Already have an account? <Link to="/Signin" className='connect'>Log in</Link> </p>
         </div>
         <SecondF />
     </div>
