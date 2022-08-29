88a01a14b2938a7121eccead6e5faa3f3d18387c
mbarndouka <mmbarndouka@gmail.com>
2022-08-29 08:31:40 +0200

    Sponsor new

--- src/ui/pages/SignupAsponsor/Sponsor.jsx
+++ src/ui/pages/SignupAsponsor/Sponsor.jsx
@@ -1,10 +1,20 @@
 import './signupsponsor.css'
+import SecondF from '../../components/Secondfooter/SecondF';
+import Logos from '../../../assets/Vector2.svg';
 
 
-export default function Signupsponsor() {
+export default function Sponsor() {
   return (
-    <div>
-        signupsponsor
+    <div className='sponsor'>
+      <img src={Logos} alt="logo-company" className='logo33'/>
+      <form action="" className='retrive'>
+        <h1>Sign up to start helping out</h1>
+        <div className='googleapplle'>
+          
+        </div>
+        <p className='line'>or</p>
+      </form>
+        <SecondF />
     </div>
   )
 }
