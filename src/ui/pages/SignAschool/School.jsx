c62e111137c55ca8e7d322ff169db1cd55b4559e
mbarndouka <mmbarndouka@gmail.com>
2022-09-16 08:34:36 +0200

    signup as school

--- src/ui/pages/SignAschool/School.jsx
+++ src/ui/pages/SignAschool/School.jsx
@@ -6,11 +6,39 @@ import Logos from '../../../assets/Vector2.svg';
 import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import Buttongoo from '../../components/buttongoogle/index';
-import ButtonApp from '../../components/buttonapple/index'
+import ButtonApp from '../../components/buttonapple/index';
+import getFormData from '../../../utils/get-form-data';
+import { UserRepositoryImpl } from '../../../Data/repository/user-repository';
 
 export default function School() {
 
     const [showPassword, setShowPassword] = useState("");
+
+    const userRepository = new UserRepositoryImpl();
+  /**
+   * @param {Event} evt
+  */
+
+    const submitUserData = async (evt)=>{
+      evt.preventDefault();
+      /**
+       * @param {HTMLFormElement}
+      */
+  
+      const form = evt.target;
+  
+      /**
+       * @type {import('../../../Data/models/types').UserModel}
+      */
+  
+      const formData = getFormData(form);
+      // console.log("okk");
+      console.log(formData);
+      // setIsloading(true);
+      const resp = await userRepository.registerUser(formData);
+      console.log("response: ", resp);
+    }
+  
 
     const handleShowPassword =(e)=>{
         e.preventDefault();
@@ -19,17 +47,17 @@ export default function School() {
   return (
     <div className='School'>
         <img src={Logos} alt="logo-company" className='logo33'/>
-        <form action="" className='retrive'>
+        <form onSubmit={submitUserData} action="POST" className='retrive'>
             <h1>Sign up to be put in touch with a donor</h1>
             <div className='googleapplle'>
               <Buttongoo />
               <ButtonApp />
             </div>
                 <p className='line'>or</p>
-                <input type="text" className='retrive-1' placeholder='Name of the school'/>
-                <input type="email" className='retrive-3' placeholder='Work email address'/>
+                <input type="text" name='firstname'className='retrive-1' placeholder='Name of the school'/>
+                <input type="email" name='email' className='retrive-3' placeholder='Work email address'/>
                 <div className="handleP">
-                    <input type={showPassword ? "text" : "password"} className='retrive-4' placeholder='Password'/>
+                    <input type={showPassword ? "text" : "password"} name='password' className='retrive-4' placeholder='Password'/>
                     {showPassword ? (
                         <div className="icon">
                             <AiIcons.AiOutlineEye
@@ -45,8 +73,10 @@ export default function School() {
                         /></div>
                 )}
         </div>
-        <select id='country' value='country' className='Select1'>
-          <option value="Rwanda" selected>Rwanda</option>
+        <input type="hidden" name="roles" value="school" />
+        <input type="hidden" name="secondName" value="schoolName" />
+        <select id='country' name='location' className='Select1'>
+          <option value="Rwanda" >Rwanda</option>
           <option value="Tanzania">Tanzania</option>
           <option value="Burundy">Burundy</option>
         </select>
